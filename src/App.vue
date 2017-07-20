546ff18114c1fcc8c4a3cbae8179100968ec09eb
hfercc <137133744@qq.com>
2017-07-20 17:16:54 +0800

    Use axios. Almost finish login.vue(not test yet)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/brand.png" width="500px">
     <router-view></router-view>
   </div>
 </template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="container" style="min-width:1200px;">
     <div class="row">
-      <div class="col-xs-8 ">
-        <img src="../assets/logo.png">
-      </div>
-      <div class="col-xs-4">
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4" style="padding-left:20px;padding-right:20px;">
         <form style="margin-top:50px;" class="biohub-form">
           <div class="form-group">
             <div class="input-group">
               <div class="input-group-addon"><i class="fa fa-user fa-fw"></i></div>
-              <input type="text" class="form-control" id="usernameInput" placeholder="Username">
+              <input type="text" class="form-control" id="usernameInput" placeholder="Username" v-model="username">
             </div>
           </div>
           <div class="form-group">
             <div class="input-group">
               <div class="input-group-addon"><i class="fa fa-key fa-fw"></i></div>
-              <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+              <input type="password" class="form-control" id="passwordInput" placeholder="Password" v-model="password">
             </div>
           </div>
-          <button type="submit" class="btn btn-default btn-biohub">Sign in</button>
+          <button type="submit" class="btn btn-default btn-biohub" @click.prevent="Login">Sign in</button>
         </form>
       </div>
+      <div class="col-xs-4"></div>
     </div>
   </div>
 </template>
 <script>
+  import axios from 'axios'
   export default {
-    name: 'login'
+    name: 'login',
+    data () {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      Login () {
+        console.log(this.username)
+        console.log(this.password)
+        axios.post('http://localhost:10800/api/users/login/', {
+          username: this.username,
+          password: this.password
+        }).then((response) => {
+          console.log(response)
+        })
+      }
+    }
   }
 </script>
