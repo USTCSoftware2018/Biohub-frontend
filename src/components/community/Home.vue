f901b317ba8f2107cab1fc4817b0f3f212c9cb58
hfercc <137133744@qq.com>
2017-07-21 16:22:55 +0800

    create directory 'community'.create topNavbar.vue'

---
+++ src/components/community/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <top-navbar></top-navbar>
+</template>
+
+<script>
+  import topNavbar from '../topNavbar.vue'
+
+  export default {
+    components: {
+      topNavbar
+    }
+  }
+</script>
---
+++ src/components/topNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">
+          <img alt="Brand" src="">
+        </a>
+      </div>
+
+      <ul class="nav navbar-nav">
+        <li>Link 1</li>
+        <li>link 2</li>
+        <li>Link 3</li>
+      </ul>
+
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search...">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
