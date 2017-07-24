1bccb8a3f7cb018d844c67db0fc47431ca0e85ac
hfercc <137133744@qq.com>
2017-07-24 16:19:01 +0800

    daily commit.

--- src/components/community/Repo.vue
+++ src/components/community/Repo.vue
@@ -1,5 +1,32 @@
 <template>
-  <top-navbar></top-navbar>
+  <div>
+    <top-navbar></top-navbar>
+    <div class="row">
+      <div class="container">
+        <div class="col-md-9">
+          <ul class="nav nav-tabs nav-repo">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <div class="panel panel-default panel-biohub">
+            <div class="panel-heading">About the author</div>
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+          <ul class="list-group left-list">
+            <li class="list-group-header">Devices which use this part...</li>
+            <li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+            <li class="list-group-item"><a href="#">consectetur adipisicing elit</a></li>
+            <li class="list-group-item"><a href="#">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ src/components/community/RepoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="repo-wrapper" v-bind:class="{repo_home: insideHome, repo_profile: insideProfile}">
+    <ul class="list-group">
+      <li class="list-group-header">Your Parts & Devices</li>
+      <li class="list-group-item"><a href="#">Cras justo odio</a></li>
+      <li class="list-group-item"><a href="#">Dapibus ac facilisis in</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['inside'],
+    data () {
+      return {
+        sInside: this.inside,
+        insideHome: false,
+        insideProfile: false
+      }
+    },
+    mounted () {
+      if (this.sInside === 'home') {
+        this.insideHome = true
+      } else {
+        this.insideProfile = true
+      }
+    }
+  }
+</script>
