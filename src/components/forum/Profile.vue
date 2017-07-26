e33dd19870a5a9afc42a09ff9c208e2a49f7248d
gloit <gloit042@gmail.com>
2017-07-26 18:14:54 +0000

    update profile

--- src/components/forum/Profile.vue
+++ src/components/forum/Profile.vue
@@ -4,15 +4,21 @@
       <div class="col-md-3 profile-left">
         <div class="profile-avatar-container">
           <div class="profile-avatar">
-            <img src="https://www.gravatar.com/avatar/d8cc2ce518e0df9a75316d124c2fc057?s=200&r=g&d=identicon">
+            <img src="https://www.gravatar.com/avatar/d8cc2ce518e0df9a75316d124c2fc057?s=328&r=g&d=identicon">
           </div>
         </div>
         <div id="author-info">
           <h1> {{ $route.params.author }} </h1>
-          <div>
-            <a class="profile-follower-num" href="#">55</a>
-            <a class="profile-follower" href="#">followers</a>
-          </div>
+
+        </div>
+      </div>
+      <div class="col-md-1"></div>
+      <div class="col-md-6">
+        <div class="outer-container">
+            <div class="profile-biography">
+              Lorem ipsum dolor sit amet, eos ei mnesarchum moderatius, ea qui aeque ridens graeci. Cum suas sale aliquando ex, mel latine sapientem cu, sumo iusto gloriatur qui an. In mel prima corpora delectus, quo periculis vituperata efficiantur ut, no usu simul soleat. No dicant tantas hendrerit pro, cum ei mandamus elaboraret, sint salutandi vituperatoribus vim an.
+              Vis eu nibh omnis.
+            </div>
         </div>
       </div>
     </div>
@@ -29,21 +35,38 @@
     font-size: 40px !important;
   }
 
-
   #author-info div a {
-    font-family:Helvetica;
+    font-family: Helvetica;
     font-weight: 800;
     text-decoration: none;
   }
 
-  .profile-follower-num {
-    font-size: 30px;
+  .profile-biography {
+    position: absolute;
+    top:0;
+    bottom:0;
+    left:0;
+    right:-17px;
+    color: gray;
+    font-size: 17px;
+    text-align: left;
+    overflow-y: scroll;
   }
 
-  .profile-follower {
-    font-size:20px;
+  .outer-container {
+    margin-top: 10%;
+    height: 170px;
+    width: 100%;
+    overflow: hidden;
+    position: relative;
   }
 
+  .inner-container::-webkit-scrollbar {
+    display: none;
+  }
 
+  .profile {
+    margin-top: 5%;
+  }
 
 </style>
