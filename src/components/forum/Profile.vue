d9a8b4426fb96d6cf6e7cf5fc3295fcc39a02ae3
gloit <gloit042@gmail.com>
2017-07-27 15:42:41 +0000

    profile biography finish

--- src/components/forum/Profile.vue
+++ src/components/forum/Profile.vue
@@ -7,18 +7,27 @@
             <img src="https://www.gravatar.com/avatar/d8cc2ce518e0df9a75316d124c2fc057?s=328&r=g&d=identicon">
           </div>
         </div>
-        <div id="author-info">
-          <h1> {{ $route.params.author }} </h1>
-
+        <div class="author-info">
+          <div id="author-name"> {{ $route.params.author }} </div>
+          <div id="author-mail">gloit042@gmail.com</div>
         </div>
       </div>
       <div class="col-md-1"></div>
-      <div class="col-md-6">
-        <div class="outer-container">
+      <div class="col-md-7 ">
+        <div class="profile-tab">
+          About me
+        </div>
+        <div class="profile-biography-frame">
+          <div class="outer-container">
             <div class="profile-biography">
-              Lorem ipsum dolor sit amet, eos ei mnesarchum moderatius, ea qui aeque ridens graeci. Cum suas sale aliquando ex, mel latine sapientem cu, sumo iusto gloriatur qui an. In mel prima corpora delectus, quo periculis vituperata efficiantur ut, no usu simul soleat. No dicant tantas hendrerit pro, cum ei mandamus elaboraret, sint salutandi vituperatoribus vim an.
-              Vis eu nibh omnis.
+              {{ biography }}
             </div>
+          </div>
+        </div>
+        <div class="activity-banner">
+          <span>
+            Acitivity
+          </span>
         </div>
       </div>
     </div>
@@ -26,36 +35,71 @@
 </template>
 
 <script>
-  export default {}
+  var biography = 'Lorem ipsum dolor sit amet, eos ei mnesarchum moderatius, ea qui aeque ridens graeci. Cum suas sale aliquando ex, mel latine sapientem cu, sumo iusto gloriatur qui an. In mel prima corpora delectus, quo periculis vituperata efficiantur ut, no usu simul soleat. No dicant tantas hendrerit pro, cum ei mandamus elaboraret, sint salutandi vituperatoribus vim an.\n' +
+    'Vis eu nibh omnis.'
+  export default {
+    data () {
+      return {
+        biography: biography
+      }
+    }
+  }
 </script>
 
 <style>
-  #author-info h1 {
+  .author-info {
     text-align: left;
+    -ms-word-wrap: break-word;
+    word-wrap: break-word;
+  }
+
+  #author-name {
     font-size: 40px !important;
   }
 
-  #author-info div a {
-    font-family: Helvetica;
-    font-weight: 800;
-    text-decoration: none;
+  #author-mail {
+    color: gray;
+    font-size: 14px !important;
+  }
+
+  .profile-tab {
+    margin-top: 2%;
+    width: 100px;
+    padding: 5px 0 5px 0;
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2px;
+    border-radius: 2px;
+    border: 1px solid #ddd;
+    border-bottom: 0px;
+  }
+
+  .activity-banner {
+    margin-top: 22px;
+    font-size: 34px;
+  }
+
+  .profile-biography-frame {
+    background: white;
+    padding: 10px;
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2px;
+    border-radius: 2px;
+    border: 1px solid #ddd;
   }
 
   .profile-biography {
     position: absolute;
-    top:0;
-    bottom:0;
-    left:0;
-    right:-17px;
-    color: gray;
-    font-size: 17px;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: -17px;
+    font-size: 14px;
     text-align: left;
     overflow-y: scroll;
   }
 
   .outer-container {
-    margin-top: 10%;
-    height: 170px;
+    height: 140px;
     width: 100%;
     overflow: hidden;
     position: relative;
