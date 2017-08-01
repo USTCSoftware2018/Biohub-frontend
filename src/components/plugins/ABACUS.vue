ef0980950ec0f6816852c5f30b5b8346c01152a5
hfercc <137133744@qq.com>
2017-08-01 12:22:10 +0800

    start ABACUS.vue

--- src/components/plugins/ABACUS.vue
+++ src/components/plugins/ABACUS.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>ABACUS</p>
+  <div style="background-color: white;border: 1px solid #ddd; border-top: none;">
+    <div class="plugin-description">
+      <p class="plugin-header">Amino Acid Sequence Designer</p>
+    </div>
+    <div class="plugin-container">
+      <p>This is a plugin to design amino acid sequence automatically according to the .pdb files uploaded by its users.</p>
+      <p class="step">STEP 1.</p>
+    </div>
+  </div>
 </template>
 
 <script>
