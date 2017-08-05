b879c7f27601cb7f6fdd25ef63530c3e09444675
kongyj <kyj1996@mail.ustc.edu.cn>
2017-08-05 17:02:05 +0800

    add bbkmanage.vue

---
+++ src/components/bbkManager.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <component v-if="show" is="plugin"></component>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default {
+    name: 'hello',
+    mounted () {
+      var link = document.createElement('script')
+      link.src = 'http://localhost:8000/plugin.js'
+      document.body.appendChild(link)
+      window.onload = () => {
+        let plugin = window.plugin
+        Vue.component('plugin', plugin)
+        this.show = true
+      }
+    },
+    data () {
+      return {
+        msg: 'Biohub Init',
+        show: false
+      }
+    }
+  }
+</script>
