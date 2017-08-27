f5452c6b342039bedd6352d919d0d8d24922f02e
gloit <gloit042@gmail.com>
2017-08-27 00:30:46 +0000

    daily commit

--- src/components/forum/Repo/New.vue
+++ src/components/forum/Repo/New.vue
@@ -1,30 +1,49 @@
 <template>
-  <div>
-    <form class="biohub-form forum-new-form">
-      <div class="form-group">
-        <div style="vertical-align: middle;text-align: justify; padding-left: 2.31rem;margin-bottom: 0.05rem;font-size: 0.2rem;">Part Name</div>
-        <input class="form-control" placeholder="name" v-model="newPartName" v-on:keydown.enter.prevent="search" style="display: inline-block;vertical-align: middle;">
-        <span class="fa fa-spinner fa-spin fa-fw" style="vertical-align: middle" v-show="stateLoad"></span>
-        <span class="fa fa-check-circle" style="vertical-align: middle" v-show="stateFound">Result Found.</span>
-        <span class="fa fa-exclamation-circle" style="vertical-align: middle" v-show="stateNotFound"></span>
+  <div class="container">
+    <div class="col-md-12">
+      <form class="biohub-form forum-new-form">
+        <div class="form-group">
+          <div
+            style="vertical-align: middle;margin-bottom: 0.05rem;font-size: 0.2rem;">
+            Part Name
+          </div>
+          <div class="input-group">
+            <div class="input-group-addon">BBa_</div>
+            <input class="form-control" placeholder="name" v-model="newPartName"
+                   v-on:keydown.enter.prevent="search(newPartName)"
+                   style="display: inline-block;vertical-align: middle;width: 95%;">
+            <span class="fa fa-spinner fa-spin fa-fw input-indicator" style="vertical-align: middle;" v-show="stateLoad"></span>
+            <span class="fa fa-check-circle input-indicator" style="vertical-align: middle;" v-show="stateFound">Result Found.</span>
+            <span class="fa fa-exclamation-circle input-indicator" style="vertical-align: middle;" v-show="stateNotFound"></span>
+          </div>
+        </div>
+      </form>
+      <p></p>
+      <div class="OfficialResult" v-if="stateFound">
+        <div class="OfficialResultHead">{{findResult.name}}</div>
+        <div class="divider"></div>
+        <div class="OfficialResultInfo">{{findResult.name}}<br>{{findResult.designer}}<br>
+          {{findResult.part_type}}<br>{{findResult.sequence_a}}<br>
+        </div>
+        <div class="divider"></div>
+        <div class="OfficialFooter">Is that you need?
+          <button class="btn btn-forum">Yes</button>
+          <button class="btn btn-forum">No</button>
+        </div>
       </div>
-    </form>
-    <p></p>
-    <div class="OfficialResult" v-if="stateFound">
-      <div class="OfficialResultHead">{{findResult.name}}</div>
-      <div class="divider"></div>
-      <div class="OfficialResultInfo">{{findResult.name}}<br>{{findResult.designer}}<br>
-        {{findResult.part_type}}<br>{{findResult.sequence_a}}<br>
+      <div class="UploadFile" v-if="stateNotFound">
+        <div class="UploadFileNotice">You can upload . file by yourself to start a discussion.</div>
       </div>
-      <div class="divider"></div>
-      <div class="OfficialFooter">Is that you need?<button class="btn btn-forum">Yes</button>
-        <button class="btn btn-forum">No</button></div>
-    </div>
-    <div class="UploadFile" v-if="stateNotFound">
-      <div class="UploadFileNotice">You can upload . file by yourself to start a discussion.</div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .input-indicator {
+    font-size: 2em;
+    margin-top: 0.1em;
+  }
+</style>
 
 <script>
   let timer = null
@@ -53,7 +72,7 @@
         }).catch((error) => {
           this.stateLoad = false
           this.stateNotFound = true
-          console.log(error, this.stateLoad)
+          console.log(error.response, this.stateLoad)
         })
       }
     }
