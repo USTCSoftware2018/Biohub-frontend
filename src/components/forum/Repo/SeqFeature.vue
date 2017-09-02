8158505022069dba26363ef5b6aa016543363ce2
hfercc <137133744@qq.com>
2017-09-02 14:17:53 +0800

    add tooltip

--- src/components/forum/Repo/SeqFeature.vue
+++ src/components/forum/Repo/SeqFeature.vue
@@ -1,5 +1,27 @@
 <template>
+  <div id='fContainer'>
+    <button type="button" data-toggle="tooltip" data-placement="bottom" title="Tooltip on top"></button>
+  </div>
 </template>
+
+<style scoped>
+  #fContainer {
+    position: relative;
+    width: 100%;
+    height: 20px;
+    background-color: cornflowerblue;
+    border-radius: 5px;
+    margin-top: 5px;
+  }
+  button {
+    top: 0px;
+    position: absolute;
+    height: 20px;
+    outline: none;
+    box-shadow: none;
+    border: none;
+  }
+</style>
 
 <script>
   export default {
@@ -13,6 +35,10 @@
       this.endPoint = _.result(_.find(this.feaData, (fea) => {
         return (fea.feature_type === 'stop' | fea.feature_type === 'new_feature')
       }), 'end_loc')
+      $(function () {
+        console.log($('[data-toggle="tooltip"]'))
+        $('[data-toggle="tooltip"]').tooltip()
+      })
     }
   }
 </script>
