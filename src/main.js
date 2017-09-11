cdf3c6a0377da2055297730ca358cc278c5b4f68
hfercc <137133744@qq.com>
2017-09-11 19:52:32 +0800

    add 'import'

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import store from './store'
 import $ from 'jquery'
 import axios from 'axios'
 import _ from 'lodash'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
-Vue.use(Vuex)
 Vue.config.productionTip = false
 axios.get('/api/users/me/').then((response) => {
   Vue.prototype.USER = response.data
@@ -18,6 +17,7 @@ axios.get('/api/users/me/').then((response) => {
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
