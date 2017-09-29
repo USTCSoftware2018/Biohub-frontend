b620de88fa661cff5c3718fc81ce9ffa37764ae4
hfercc <137133744@qq.com>
2017-09-29 19:42:25 +0800

    IGNORE Hello.vue

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,23 +2,6 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
-
-    browser.url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert
-      .elementPresent('.hello')
-      .assert
-      .containsText('h1', 'Biohub Init')
-      .assert
-      .elementCount('img', 1)
-      .end()
-  },
-
   'home tests': function (browser) {
     browser.url('http://localhost:8080')
       .waitForElementVisible('#app', 5000)
